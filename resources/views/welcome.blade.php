f2ecdc5b03017f3b3110628f3562caf1a30732ca
okursoftware <okurokur2014@gmail.com>
2019-03-13 22:58:59 +0300

    master - bey

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Orhan Metin git ögreniyor..
+                    Master bey diyeceksiniz..
                 </div>
 
                 <div class="links">
