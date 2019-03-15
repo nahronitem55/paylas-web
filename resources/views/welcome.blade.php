4218b80f01c4257eaad289769c1df95433b6c244
nahronitem55 <orhan.metin55@gmail.com>
2019-03-15 18:55:14 +0300

    deneme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Orhan Metin git ögreniyor.. ver0.0.1 deneme
+                    Orhan Metin git ögreniyor..
+                    Master bey diyeceksiniz..
                 </div>
 
                 <div class="links">
